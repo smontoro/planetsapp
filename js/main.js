6a38a2a05e864dae742d5bf2dcaf851a316f32cd
smontoro <sarahjmontoro@gmail.com>
2018-02-03 16:08:25 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+
+/*Login App
+var users = [
+
+	{
+		name: "sarah",
+		pass: "dogs"
+	},
+	{
+		name: "matthew",
+		pass: "cats"
+	},
+	{
+		name: "patty",
+		pass: "birds"
+	},
+	{
+		name: "flavio",
+		pass: "turtles"
+	}
+
+]
+
+function logIn() {
+	for(i = 0; i < users.length; i++) {
+		var username = document.getElementById("username").value
+		var password = document.getElementById("password").value
+
+		if(users[i].name == username && users[i].pass == password) {
+			alert("Welome!")
+		} else {
+			alert("That is incorrect")
+		}
+		return
+	}
+}*/
+
